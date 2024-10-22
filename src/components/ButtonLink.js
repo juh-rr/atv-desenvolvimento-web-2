ca32a642adbba64ea6bc3d1093418792369edc6e
Juliana Ramalho <juhramalho.r@gmail.com>
2024-10-21 23:14:54 -0300

    botões para troca de telas

---
+++ src/components/ButtonLink.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function ButtonLink({ to, children }) {
+
+    return <Link to={to}><button>{children}</button></Link>;
+  }
+
+  export default ButtonLink
--- src/pages/tela.js
+++ src/pages/tela.js
@@ -3,11 +3,18 @@ import {Routes, Route} from 'react-router-dom'
 import './tela.css'
 import CadastroVeiculo from "./CadastroVeiculo";
 import ListaVagas from "./ListaVagas";
+import ButtonLink from "../components/ButtonLink";
 
 function Tela() {
+    
+
     return (
         <div>
             <h1> Tela inicial</h1>
+            <div className="botoes">
+                <ButtonLink to ="/cadastro">Cadastro de vagas</ButtonLink>
+                <ButtonLink to ="/lista">Lista de vagas </ButtonLink>
+            </div>
             <Routes>
                 <Route path='/cadastro' element={<CadastroVeiculo />} />
                 <Route path='/lista' element={<ListaVagas />} />                 
